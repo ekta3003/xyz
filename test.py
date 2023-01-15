f3138bcc6ae010b8c46c13c691787fe4684e10d4
BladesInfinite <90135181+BladesInfinite@users.noreply.github.com>
2023-01-15 22:15:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 x = requests.get('https://tsneh.vercel.app/hs9bygr7au91')
 
-with open("3760393314e0a5b393ad60eb24565ecd.m3u", "w") as f:
+with open("test.m3u", "w") as f:
     f.write(x.text)
